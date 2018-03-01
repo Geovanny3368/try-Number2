d6d9d26e2c6e065ab78c107755d97a5316fca634
Geovanny <caster_boy@icloud.com>
2018-03-01 15:27:39 -0700

    This is me adding a comment

--- app/src/main/java/com/example/galaxy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/galaxy/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     //This is new to me. I am messing with git and github through cmd.
+    //The second push to the server is now using an ssh key
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
