5fd4869a1ae1cdd6b1125aa9331bb4bc5f674dc7
Geovanny <caster_boy@icloud.com>
2018-03-01 15:57:58 -0700

    This is the push using git bash through the terminal

--- app/src/main/java/com/example/galaxy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/galaxy/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     //This is new to me. I am messing with git and github through cmd.
     //The second push to the server is now using an ssh key
+    //The push with the ssh key did not work because the path was configured with Htts://
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
